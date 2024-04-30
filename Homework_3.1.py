51cdf6ac7a6e5a674d2781fd3a1aafa2a3cf292e
Vladislav Prokopchuk <pro90015@rambler.ru>
2024-05-01 00:08:30 +0300

    change1

--- Homework_3.1.py
+++ Homework_3.1.py
@@ -1,47 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-from fake_headers import Headers
 import json
+url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
+keywords = ['Django', 'Flask']
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/58.0.3029.110 ',
+}
+response = requests.get(url, headers=headers)
 
-# определяю список ключевых слов
-KEYWORDS = ['python', 'Москва', 'Санкт-Петербург', 'Django', 'Flask']
-MAIN_LINK = 'https://spb.hh.ru/search/vacancy'
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    vacancies = soup.find_all(class_='vacancy-serp-item')
+    results = []
+    for vacancy in vacancies:
+        link = vacancy.find('a', class_='bloko-link')['href']
+        company = vacancy.find('a', class_='bloko-link').text
+        city = vacancy.find(class_='vacancy-serp-item__meta-info').text
+        salary = vacancy.find(class_='vacancy-serp-item__compensation')
+        if salary:
+            salary = salary.text.strip()
+        else:
+            salary = 'Не указана'
+        description = vacancy.find(class_='g-user-content').text
+        if any(keyword.lower() in description.lower() for keyword in keywords):
+            vacancy_info = {
+                'link': link,
+                'company': company,
+                'city': city,
+                'salary': salary
+            }
+            results.append(vacancy_info)
 
-headers = Headers(os='win', headers=True).generate()
-ret = requests.get(MAIN_LINK, headers=headers)
-
-soup = BeautifulSoup(ret.text, 'html.parser')
-articles = soup.findAll("article", {"class": "tm-articles-list__item"})
-
-article_dicts = []
-for article in articles:
-	# ищем совпадение по ключевым словам
-	hubs = article.findAll("a", {"class": "tm-article-snippet__hubs-item-link"})
-	hubs = [hub.span.text.lower() for hub in hubs]
-	is_match = False
-	matching = [hub for hub in hubs for key in KEYWORDS if key in hub]
-	if len(matching):
-		is_match = True
-
-	if is_match:
-		title = article.find("a", {"class": "tm-article-snippet__title-link"})
-		date = article.find("span", {"class":"tm-article-snippet__datetime-published"})
-		article_dicts.append({
-			"title":title.span.text,
-			"ref":MAIN_LINK+title["href"],
-			"datetime":date.time["datetime"],
-			"matching":matching
-			})
-
-
-for article in article_dicts:
-	print(f"{article['datetime']} - {article['title']} - {article['ref']}")
-	print(f"matched words: {article['matching']}")
-	print()
-
-
-
-with open('article_dicts', 'w') as file:
-    json.dump(article_dicts, file, ensure_ascii=False, indent=4)
-
+    with open('vacancies.json', 'w', encoding='utf-8') as file:
+        json.dump(results, file, ensure_ascii=False, indent=4)
 
